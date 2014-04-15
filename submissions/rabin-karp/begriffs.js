c3dbbb219259057a61c96678e1d38be7ace31204
Joe Nelson <cred+github@begriffs.com>
2014-04-15 12:53:19 -0700

    Rabin-Karp string search

---
+++ submissions/rabin-karp/begriffs.js
@@ -0,0 +1,29 @@
+var H = require('../hash/begriffs.js')();
+
+module.exports = function () {
+
+  var hashBuckets = 1000;
+
+  return self = {
+
+    find: function(needle, haystack) {
+      var i
+        , m            = needle.length
+        , n            = haystack.length
+        , signature    = H.hash(hashBuckets, needle)
+        , searchWindow = haystack.slice(0, m)
+        , searchHash   = H.hash(hashBuckets, searchWindow);
+
+      for(i = 0; i < n-m; i++) {
+        searchWindow = haystack.slice(i, i+m);
+        if(signature === searchHash) {
+          if(needle === searchWindow) { return i; }
+        }
+        searchHash = H.roll(hashBuckets, searchHash, searchWindow, haystack[i+m]);
+      }
+
+      return -1;
+    }
+
+  };
+};
