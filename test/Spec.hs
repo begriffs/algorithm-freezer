80d2013e0317a9f336ac5f2b2bda77128cd7c755
Joe Nelson <cred+github@begriffs.com>
2015-12-26 11:12:25 -0800

    Fix warnings in test

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,7 +6,6 @@ import qualified Data.HashSet as H
 import qualified Data.Vector as V
 import Data.Char (intToDigit)
 import Data.List (sort)
-import Data.Maybe (fromJust)
 
 import Answers
 import Types
@@ -17,13 +16,13 @@ main = hspec $ do
     it "There is none in empty array" $
       mostFreq ([]::[Int]) `shouldBe` S.empty
     it "Chooses value from length 1 array" $
-      mostFreq [0] `shouldBe` S.singleton 0
+      mostFreq [0] `shouldBe` S.singleton (0::Int)
     it "Picks a simple winner" $
-      mostFreq [0,0,1] `shouldBe` S.singleton 0
+      mostFreq [0,0,1] `shouldBe` S.singleton (0::Int)
     it "Picks a simple winner in different order" $
-      mostFreq [0,1,1] `shouldBe` S.singleton 1
+      mostFreq [0,1,1] `shouldBe` S.singleton (1::Int)
     it "Detects a tie" $
-      mostFreq [0,1] `shouldBe` S.fromList [0,1]
+      mostFreq [0,1] `shouldBe` S.fromList ([0,1]::[Int])
 
   describe "Pairs adding to ten" $ do
     let tenners = adders 10
@@ -39,47 +38,47 @@ main = hspec $ do
 
   describe "Detecting when one list is a rotation of another" $ do
     it "false for unequal lengths" $
-      cycleEq [0,1] [] `shouldBe` False
+      cycleEq [0::Int,1] [] `shouldBe` False
     it "true for a typical example" $
-      cycleEq [1,2,3,5,6,7,8] [5,6,7,8,1,2,3] `shouldBe` True
+      cycleEq [1::Int,2,3,5,6,7,8] [5,6,7,8,1,2,3] `shouldBe` True
     it "true for lists with repeating elements" $
-      cycleEq [1,1,1,1,2] [1,1,1,2,1] `shouldBe` True
+      cycleEq [1::Int,1,1,1,2] [1,1,1,2,1] `shouldBe` True
     it "false for a strict sublist" $
-      cycleEq [1] [1,1] `shouldBe` False
+      cycleEq [1::Int] [1,1] `shouldBe` False
 
   describe "Items which occur only once in array" $ do
     it "Finds none in empty list" $
       loners ([]::[Int]) `shouldBe` S.empty
     it "Finds none when all are duped" $
-      loners [0,0] `shouldBe` S.empty
+      loners [0::Int,0] `shouldBe` S.empty
     it "Identifies a single loner" $
-      loners [0,1,1] `shouldBe` S.singleton 0
+      loners [0::Int,1,1] `shouldBe` S.singleton 0
     it "Identifies two loners" $
-      loners [0,1,1,2] `shouldBe` S.fromList [0,2]
+      loners [0::Int,1,1,2] `shouldBe` S.fromList [0,2]
 
   describe "Finding elements in common" $ do
     it "There are none for disjoint lists" $
-      commonElts [0] [1] `shouldBe` S.empty
+      commonElts [0::Int] [1] `shouldBe` S.empty
     it "Works as advertised" $
-      commonElts [0,1,2] [1,2,3] `shouldBe` S.fromList [1,2]
+      commonElts [0::Int,1,2] [1,2,3] `shouldBe` S.fromList [1,2]
 
   describe "Binary search on sorted array" $ do
     it "Finds nothing in an empty list" $
-      searchSorted 0 V.empty `shouldBe` Nothing
+      searchSorted (0::Int) V.empty `shouldBe` Nothing
     it "Finds a value in a singleton list" $
-      searchSorted 0 (V.singleton 0) `shouldBe` Just 0
+      searchSorted (0::Int) (V.singleton 0) `shouldBe` Just 0
     it "Finds nothing in a bad singleton list" $
-      searchSorted 0 (V.singleton 1) `shouldBe` Nothing
+      searchSorted (0::Int) (V.singleton 1) `shouldBe` Nothing
     it "Finds a value on the extreme left" $
-      searchSorted 0 (V.fromList [0,1]) `shouldBe` Just 0
+      searchSorted (0::Int) (V.fromList [0,1]) `shouldBe` Just 0
     it "Finds a value on the extreme right" $
-      searchSorted 1 (V.fromList [0,1]) `shouldBe` Just 1
+      searchSorted (1::Int) (V.fromList [0,1]) `shouldBe` Just 1
     it "Finds middle index in a repeated list" $
-      searchSorted 0 (V.fromList [0,0,0]) `shouldBe` Just 1
+      searchSorted (0::Int) (V.fromList [0,0,0]) `shouldBe` Just 1
     it "Fails to find value larger than all entries" $
-      searchSorted 2 (V.fromList [0,1]) `shouldBe` Nothing
+      searchSorted (2::Int) (V.fromList [0,1]) `shouldBe` Nothing
     it "Fails to find value smaller than all entries" $
-      searchSorted 0 (V.fromList [1,2]) `shouldBe` Nothing
+      searchSorted (0::Int) (V.fromList [1,2]) `shouldBe` Nothing
 
     -- TODO: better constrain Arbitrary instances of l and i
     describe "Properties" $
@@ -94,15 +93,15 @@ main = hspec $ do
 
   describe "Binary search on rotated array" $ do
     it "Finds nothing in an empty list" $
-      searchRotated 0 V.empty `shouldBe` Nothing
+      searchRotated (0::Int) V.empty `shouldBe` Nothing
     it "Finds a value in a singleton list" $
-      searchRotated 0 (V.singleton 0) `shouldBe` Just 0
+      searchRotated (0::Int) (V.singleton 0) `shouldBe` Just 0
     it "Finds nothing in a bad singleton list" $
-      searchRotated 0 (V.singleton 1) `shouldBe` Nothing
+      searchRotated (0::Int) (V.singleton 1) `shouldBe` Nothing
     it "Fails to find value larger than all entries" $
-      searchRotated 100 (V.fromList [2,0,1]) `shouldBe` Nothing
+      searchRotated (100::Int) (V.fromList [2,0,1]) `shouldBe` Nothing
     it "Fails to find value smaller than all entries" $
-      searchRotated (-100) (V.fromList [2,0,1]) `shouldBe` Nothing
+      searchRotated (-100::Int) (V.fromList [2,0,1]) `shouldBe` Nothing
 
     describe "Properties" $ do
       let rotate i = (\(a,b) -> b V.++ a) . V.splitAt i
